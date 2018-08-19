bb811c9a3bbeae19756e48f9406fc457479ab03e
Chen Zhen <cz.worker@gmail.com>
2018-08-19 16:24:55 +0800

    add api.js to test oauth

---
+++ webroot/js/api.js
@@ -0,0 +1,91 @@
+function request_callback_fail_failback(err) {
+    if (err.errMsg) {
+        alert('error: [#{0}] {1}'.format(err.errCode, err.errMsg));
+    }
+ }
+
+function request(url, data, method, success, fail) {
+
+    if (data && method == 'GET') {
+        data = undefined;
+    }
+
+    fail = fail || request_callback_fail_failback;
+
+    var callback_success = function (response) { //{data, statusCode, header}
+        // console.log(response.header);
+
+        var data = response;
+        console.log('[http] response data:', data);
+        if (data.errCode === undefined || data.errCode == 0) {
+            // 处理成功返回数据
+            success && success.call(null, data);
+        } else {
+            // 处理失败信息
+            fail.call(null, data); // { errCode, errMsg }
+        }
+    }
+
+    var callback_fail = function() {
+        var error = {
+            errCode: -1,
+            errMsg: "IOERROR",
+        };
+
+        fail.call(null, error);
+    }
+
+    var callback_complete = function() {
+        // code here if necessary
+    }
+
+    // ref: https://api.jquery.com/jquery.post/
+    // var req = $.post(url, JSON.stringify(data), null, 'json');
+    var req = $.ajax({
+        url: url,
+        data: JSON.stringify(data),
+        type: 'POST',
+        dataType: 'json',
+        xhrFields: {
+            withCredentials: true,
+        },
+        success: callback_success,
+        fail: callback_fail,
+        complete: callback_complete,
+    });
+}
+
+var SERVER_HOST = '/api/';
+
+/** 向api发送请求 */
+function api_request(api, data, req_callback) {
+  var url = SERVER_HOST + api;
+  req_callback = req_callback || {};
+  request(url, data, 'POST', req_callback.success, req_callback.fail);
+}
+
+var API = (function() {
+
+    ////////////http request//////////////////////////////
+
+    ///////////////////////////
+
+    function API() {
+
+    }
+
+    // 开始oauth验证
+    API.prototype.oauth = function(callback) {
+        var _this = this;
+
+        api_request('en/oauth', null, {
+            success: callback && callback.success,
+            fail: callback && callback.fail,
+        });
+    }
+    return API;
+})();
+
+
+
+
--- webroot/js/en.js
+++ webroot/js/en.js
@@ -1,7 +1,20 @@
+
+var api = new API()
+
 function start() {
     console.log('app start')
     $('#btn_oauth').on('click', function() {
-        console.log('click')
+        api.oauth({
+            success: function(data) {
+                console.log('oauth:', data)
+                if (data.authorization_url) {
+                    window.location = data.authorization_url
+                }
+            },
+            fail: function() {
+                alert('auth failed')
+            }
+        })
     })
 }
 
